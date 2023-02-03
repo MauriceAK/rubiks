8f592f4ef42cc799a200d7baaabd31abe59904d5
MauriceAK <mkenon01@gmail.com>
2023-02-03 01:43:41 -0600

    Green Light    |

--- rubik/model/cube.py
+++ rubik/model/cube.py
@@ -22,6 +22,26 @@ class Cube:
         rotatedCubeList[FTL] = cubeList[FBL]
         rotatedCubeList[FML] = cubeList[FBM]
         rotatedCubeList[FBL] = cubeList[FBR]
+
+        #rotate up to right
+        rotatedCubeList[RTL] = cubeList[UBL]
+        rotatedCubeList[RML] = cubeList[UBM]
+        rotatedCubeList[RBL] = cubeList[UBR]
+        
+        #rotate right to bottom
+        rotatedCubeList[DTR] = cubeList[RTL]
+        rotatedCubeList[DTM] = cubeList[RML]
+        rotatedCubeList[DTL] = cubeList[RBL]
+        
+        #rotate bottom to left
+        rotatedCubeList[LTR] = cubeList[DTL]
+        rotatedCubeList[LMR] = cubeList[DTM]
+        rotatedCubeList[LBR] = cubeList[DTR]
+        
+        #rotate left to up
+        rotatedCubeList[UBR] = cubeList[LTR]
+        rotatedCubeList[UBM] = cubeList[LMR]
+        rotatedCubeList[UBL] = cubeList[LBR]
         
         self.cube = "".join(rotatedCubeList)
         return self.cube
