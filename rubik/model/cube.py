16ade2e3264d87341d11dea75c4bbc273c2aed2b
MauriceAK <mkenon01@gmail.com>
2023-02-03 01:46:31 -0600

    Refactor       |

--- rubik/model/cube.py
+++ rubik/model/cube.py
@@ -6,12 +6,19 @@ class Cube:
     '''
 
     def __init__(self, encodedCube):
-        self.cube = encodedCube
-        
+        self._cube = encodedCube
+    
+    def get(self):
+        return self.cube
+  
     def rotate(self, directions):
+        self._rotateF()
+        return self._cube
+    
+   
+    def _rotateF(self):
         cubeList = list(self.cube)
         rotatedCubeList = cubeList[:]
-        
         #rotate front
         rotatedCubeList[FTR] = cubeList[FTL]
         rotatedCubeList[FMR] = cubeList[FTM]
@@ -22,30 +29,22 @@ class Cube:
         rotatedCubeList[FTL] = cubeList[FBL]
         rotatedCubeList[FML] = cubeList[FBM]
         rotatedCubeList[FBL] = cubeList[FBR]
-
         #rotate up to right
         rotatedCubeList[RTL] = cubeList[UBL]
         rotatedCubeList[RML] = cubeList[UBM]
         rotatedCubeList[RBL] = cubeList[UBR]
-        
         #rotate right to bottom
         rotatedCubeList[DTR] = cubeList[RTL]
         rotatedCubeList[DTM] = cubeList[RML]
         rotatedCubeList[DTL] = cubeList[RBL]
-        
         #rotate bottom to left
         rotatedCubeList[LTR] = cubeList[DTL]
         rotatedCubeList[LMR] = cubeList[DTM]
         rotatedCubeList[LBR] = cubeList[DTR]
-        
         #rotate left to up
         rotatedCubeList[UBR] = cubeList[LTR]
         rotatedCubeList[UBM] = cubeList[LMR]
         rotatedCubeList[UBL] = cubeList[LBR]
-        
-        self.cube = "".join(rotatedCubeList)
-        return self.cube
-    
-    def get(self):
-        return self.cube
+        self._cube = "".join(rotatedCubeList)
+
         
