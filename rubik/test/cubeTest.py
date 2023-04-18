86b01368b5a036736d4f535e9ef581d76a650f36
MauriceAK <mkenon01@gmail.com>
2023-04-18 16:53:42 -0500

    Green Light    |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -86,6 +86,6 @@ class CubeTest(unittest.TestCase):
         cubeToRotate = 'ygrwbgyggbobrryrrwobrggrgwywyryorbbggwwbyybwwooyowbooo'
         theCube = cube.Cube(cubeToRotate)
         rotatedCube = theCube.rotate('Q')
-        self.assertEqual(rotatedCube, 'DirException occurred: Invalid Direction')
+        self.assertEqual(rotatedCube, 'DirException occurred')
         #self.assertRaises(DirException, theCube.rotate('Z'))
     
