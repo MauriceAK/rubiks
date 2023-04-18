beab4e207439ade5196173aecd13cae1b34c6369
MauriceAK <mkenon01@gmail.com>
2023-04-18 16:53:50 -0500

    Green Light    |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -86,6 +86,6 @@ class CubeTest(unittest.TestCase):
         cubeToRotate = 'ygrwbgyggbobrryrrwobrggrgwywyryorbbggwwbyybwwooyowbooo'
         theCube = cube.Cube(cubeToRotate)
         rotatedCube = theCube.rotate('Q')
-        self.assertEqual(rotatedCube, 'DirException occurred')
+        self.assertEqual(rotatedCube, 'DirException')
         #self.assertRaises(DirException, theCube.rotate('Z'))
     
