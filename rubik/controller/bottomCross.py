8b7ce6985b655b44dd295eda428ba1fc8e4dad82
MauriceAK <mkenon01@gmail.com>
2023-04-18 17:59:04 -0500

    Red Light      |

--- rubik/controller/bottomCross.py
+++ rubik/controller/bottomCross.py
@@ -15,9 +15,9 @@ def     solveBottomCross(theCube: Cube) -> str:
         input:  an instance of the cube class
         output: the rotations required to transform the input cube into the down-face cross 
     '''  
-    
+    return theCube
     moves = []
-    cubeList = list(theCube.get())
+    
     
     botColor = cubeList[DMM]
     botEdges = [DTM, DML, DMR, DBM]
