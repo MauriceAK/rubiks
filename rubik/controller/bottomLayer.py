bbe2aa07719725445270e5b0afbe371d287b6f06
MauriceAK <mkenon01@gmail.com>
2023-04-19 16:10:37 -0500

    Red Light      |

--- rubik/controller/bottomLayer.py
+++ rubik/controller/bottomLayer.py
@@ -65,7 +65,7 @@ def solveBottomLayer(theCube: Cube) -> str:
         
         moves += 'U'   
         theCube._rotateU()
-        return 'x'
+        return moves + 'x'
     return 'x'
     
         
