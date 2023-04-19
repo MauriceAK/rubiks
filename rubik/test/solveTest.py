aff908e2e1f363eee3994109b7cb14080aa5738f
MauriceAK <mkenon01@gmail.com>
2023-04-19 12:30:40 -0500

    Red Light      |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -29,7 +29,7 @@ class SolveTest(TestCase):
         
     def test120_solve_bottomCrossIfValid(self):
         parms = {}
-        parms['cube'] = 'ryrbborgoyowbrygywoooogbogrbywrowywwywggygbbbgwyrwrgrb'
+        parms['cube'] = 'rrgbbwgowyogrrbobwrgyoggbooobbyorgrwbwwyywyyrogbgwwyyr'
         result = solve(parms)
         self.assertIn('status', result)
         self.assertEqual('ok', result['status'])
