3a454374fa1f3a0bca83e7538ae1e7ecc6b69ca5
MauriceAK <mkenon01@gmail.com>
2023-04-18 22:45:29 -0500

    Red Light      |

--- rubik/controller/bottomCross.py
+++ rubik/controller/bottomCross.py
@@ -54,4 +54,82 @@ def     solveBottomCross(theCube: Cube) -> str:
         theCube._rotateR()
         theCube._rotateU()
         
+    #right top edge      
+    if botColor in {theCube.get()[BML], theCube.get()[DMR], theCube.get()[FMR]}:
+        while theCube.get()[UMR] != botColor:       
+            moves +='R'
+            theCube._rotateR()
+    elif botColor in {theCube.get()[RTM], theCube.get()[RMR], theCube.get()[RML], theCube.get()[RBM]}:
+        while theCube.get()[RMR] != botColor:
+            moves +='R'
+            theCube._rotateR()
+        moves += 'uBU'
+        theCube._rotateU()
+        theCube._rotateU()
+        theCube._rotateU()
+        theCube._rotateB()
+        theCube._rotateU()           
+        
+    #back top edge     
+    if botColor in {theCube.get()[RML], theCube.get()[DTM], theCube.get()[LMR]}:   
+        while theCube.get()[UTM] != botColor:
+            moves += 'B'
+            theCube._rotateB()
+    elif botColor in {theCube.get()[FTM], theCube.get()[FMR], theCube.get()[FML], theCube.get()[FBM]}:
+        while theCube.get()[BMR] != botColor:
+            moves += 'B'
+            theCube._rotateB()
+        moves += 'uLU'
+        theCube._rotateU()
+        theCube._rotateU()
+        theCube._rotateU()
+        theCube._rotateL()
+        theCube._rotateU()  
+            
+    #left top edge  
+    if botColor in {theCube.get()[RML], theCube.get()[DTM], theCube.get()[LMR]}:      
+        while theCube.get()[UML] != botColor:
+            moves += 'L'
+            theCube._rotateL()
+    elif botColor in {theCube.get()[FTM], theCube.get()[FMR], theCube.get()[FML], theCube.get()[FBM]}:
+        while theCube.get()[LMR] != botColor:
+            moves += 'L' 
+            theCube._rotateL()
+        moves += 'uFU'
+        theCube._rotateU()
+        theCube._rotateU()
+        theCube._rotateU()
+        theCube._rotateF()
+        theCube._rotateU()  
+     
+    
+    if checkDaisy(theCube) is True:
+        while theCube.get()[FTM] != theCube.get()[FMM] or theCube.get()[UBM] != botColor:
+            moves += 'U'
+            theCube._rotateU()
+        moves += 'FF'
+        theCube._rotateF()
+        theCube._rotateF()
+        
+        while theCube.get()[RTM] != theCube.get()[RMM] or theCube.get()[UMR] != botColor:
+            moves += 'U'
+            theCube._rotateU()
+        moves +='RR'
+        theCube._rotateR()
+        theCube._rotateR()
+        
+        while theCube.get()[BTM] != theCube.get()[BMM] or theCube.get()[UTM] != botColor:
+            moves += 'U'
+            theCube._rotateU()
+        moves += 'BB'
+        theCube._rotateB()
+        theCube._rotateB()
+        
+        while theCube.get()[LTM] != theCube.get()[LMM] or theCube.get()[UML] != botColor:
+            moves += 'U'
+            theCube._rotateU()
+        moves += 'LL'
+        theCube._rotateL()
+        theCube._rotateL()
+        
     return moves
