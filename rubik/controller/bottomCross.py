427fcf90e9cc748a63aba3b0e70035de874c4144
MauriceAK <mkenon01@gmail.com>
2023-04-18 21:57:35 -0500

    Red Light      |

--- rubik/controller/bottomCross.py
+++ rubik/controller/bottomCross.py
@@ -40,9 +40,9 @@ def     solveBottomCross(theCube: Cube) -> str:
     botcolor_edges = [i for i in range(len(cubeList)) if cubeList[i] == botColor]
     
     #front top edge
-    
+    return 'X'
     if botColor == (cubeList[RML] or cubeList[DTM] or cubeList[LMR]):
-        return 'X'
+        
         while cubeList[UBM] != botColor:
             moves += 'F'
             theCube._rotateF()
