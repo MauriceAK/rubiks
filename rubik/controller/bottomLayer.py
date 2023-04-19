0eef6fd557221d443a7a6f4f96582b7b6a481ec9
MauriceAK <mkenon01@gmail.com>
2023-04-19 17:57:23 -0500

    Red Light      |

--- rubik/controller/bottomLayer.py
+++ rubik/controller/bottomLayer.py
@@ -2,31 +2,12 @@ import rubik.model.constants
 from rubik.model.constants import *
 from rubik.model.cube import Cube
 
-def solveBottomLayer(theCube: Cube) -> str:
-    '''
-        This is the top-level function  for rotating
-        a cube so that the bottom layer is solved.
-        
-        input:  an instance of the cube class with the down-face cross solved
-        output: the rotations required to solve the bottom layer  
-    '''  
-    
+def removeWhiteCorners(theCube) -> str:
     moves = ''
-    
     botColor = theCube.get()[DMM]
-    cornerWRB = [botColor, theCube.get()[FMM], theCube.get()[RMM]]
     cornerWhites = [theCube.get()[DTR], theCube.get()[FBR], theCube.get()[RBL], theCube.get()[RBR], 
                     theCube.get()[BBL], theCube.get()[DBR], theCube.get()[DBL], theCube.get()[BBR], 
                     theCube.get()[LBL], theCube.get()[FBL], theCube.get()[LBR], theCube.get()[DTL]]
-
-    
-    if theCube.get()[DTL] == theCube.get()[DBL] == theCube.get()[DTR] == theCube.get()[DBR] == botColor and \
-         theCube.get()[FBL] == theCube.get()[FBR] == theCube.get()[FMM] and \
-         theCube.get()[RBL] == theCube.get()[RBR] == theCube.get()[RMM] and \
-         theCube.get()[BBL] == theCube.get()[BBR] == theCube.get()[BMM] and \
-         theCube.get()[LBL] == theCube.get()[LBR] == theCube.get()[LMM]:
-        return moves
-    
     
     while botColor in cornerWhites:
         if botColor in [theCube.get()[DTL], theCube.get()[FBL], theCube.get()[LBR]]:
@@ -67,6 +48,52 @@ def solveBottomLayer(theCube: Cube) -> str:
                     theCube.get()[BBL], theCube.get()[DBR], theCube.get()[DBL], theCube.get()[BBR], 
                     theCube.get()[LBL], theCube.get()[FBL], theCube.get()[LBR], theCube.get()[DTL]]
         #return moves + 'x'
+    
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def solveBottomLayer(theCube: Cube) -> str:
+    '''
+        This is the top-level function  for rotating
+        a cube so that the bottom layer is solved.
+        
+        input:  an instance of the cube class with the down-face cross solved
+        output: the rotations required to solve the bottom layer  
+    '''  
+    
+    moves = ''
+    
+    botColor = theCube.get()[DMM]
+    #cornerWRB = [botColor, theCube.get()[FMM], theCube.get()[RMM]]
+    cornerWhites = [theCube.get()[DTR], theCube.get()[FBR], theCube.get()[RBL], theCube.get()[RBR], 
+                    theCube.get()[BBL], theCube.get()[DBR], theCube.get()[DBL], theCube.get()[BBR], 
+                    theCube.get()[LBL], theCube.get()[FBL], theCube.get()[LBR], theCube.get()[DTL]]
+
+    
+    if theCube.get()[DTL] == theCube.get()[DBL] == theCube.get()[DTR] == theCube.get()[DBR] == botColor and \
+         theCube.get()[FBL] == theCube.get()[FBR] == theCube.get()[FMM] and \
+         theCube.get()[RBL] == theCube.get()[RBR] == theCube.get()[RMM] and \
+         theCube.get()[BBL] == theCube.get()[BBR] == theCube.get()[BMM] and \
+         theCube.get()[LBL] == theCube.get()[LBR] == theCube.get()[LMM]:
+        return moves
+    
+    removeWhiteCorners(theCube)
+    moves += theCube.get()
     
     
     while theCube.get()[DTR] != botColor or theCube.get()[FBR] != theCube.get()[FMM] or \
