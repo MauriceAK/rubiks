745871b6af1a093cf05a466bbd6bed180b0a9e3c
MauriceAK <mkenon01@gmail.com>
2023-04-19 16:41:29 -0500

    Red Light      |

--- rubik/controller/bottomLayer.py
+++ rubik/controller/bottomLayer.py
@@ -98,7 +98,7 @@ def solveBottomLayer(theCube: Cube) -> str:
                  theCube.get()[RTR] in ['w', 'r', 'g'] and \
                  theCube.get()[UTL] in ['w', 'r', 'g']:
                 
-                return moves + 'x'
+               
                 while theCube.get()[DBR] != botColor:
                     moves += 'BUbu'
                     theCube._rotateB()
@@ -109,7 +109,7 @@ def solveBottomLayer(theCube: Cube) -> str:
                     theCube._rotateU()
                     theCube._rotateU()
                     theCube._rotateU()
-                    
+                return moves + 'x'   
         else:
                 moves += 'U'
                 theCube._rotateU()
