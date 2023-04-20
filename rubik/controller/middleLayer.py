206be7e6741c5647831be191cfc51cf3b1268a79
MauriceAK <mkenon01@gmail.com>
2023-04-20 00:45:05 -0500

    Red Light      |

--- rubik/controller/middleLayer.py
+++ rubik/controller/middleLayer.py
@@ -260,6 +260,7 @@ def solveMiddleLayer(theCube: Cube) -> str:
 
         return moves + 'x'         
     moves += 'x'
+    return moves + 'x'  
     while theCube.get()[BMM] != theCube.get()[BML] or theCube.get()[BMM] != theCube.get()[BMR] or \
         theCube.get()[LML] != theCube.get()[LMM] or \
         theCube.get()[RMR] != theCube.get()[RMM]:
